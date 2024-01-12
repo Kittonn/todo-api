06e7ff7905e54a5a2a51b577c7126f3cdd1637f4
Kittonn <tonkittipod@gmail.com>
2024-01-12 22:26:45 +0700

    feat: setup config with env module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from '../env/env';
+import { EnvModule } from '../env/env.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (config) => envSchema.parse(config),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
