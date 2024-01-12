5aece7cc4e575c91eb9e958e6add36f8c3ed89dd
Kittonn <tonkittipod@gmail.com>
2024-01-12 22:27:01 +0700

    feat: env enum

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { ConfigService } from '@nestjs/config';
+import { EnvService } from './modules/env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const port = configService.get('app.port');
+  const envService = app.get(EnvService);
+  const port = envService.get('PORT');
 
   await app.listen(port);
 }
